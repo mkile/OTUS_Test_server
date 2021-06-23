456a9401155f4c61c059f136cdaa2d27d54ca3e1
Михаил Овсянкин <mkiles81@gmail.com>
2021-06-23 17:27:43 +0300

    added few lines to server

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+from http import HTTPStatus
 import re
 
 sock = socket.socket(family=socket.AF_INET,
@@ -6,10 +7,16 @@ sock = socket.socket(family=socket.AF_INET,
                      proto=0)
 sock.bind(('127.0.0.1', 8090))
 sock.listen(1)
-conn, addr = sock.accept()
+while True:
+    conn, addr = sock.accept()
 
-print(conn, addr)
-data = conn.recv(1024)
-print('Recieved', data)
+    print(conn, addr)
+    data = conn.recv(1024)
+    print('Recieved', data)
+    protocol = 'HTTP/1.1'
+    status = str(HTTPStatus.OK.value)
+    status_code = HTTPStatus.OK.phrase
+    message = f'{protocol} {status} {status_code}'
+    conn.send(message.encode('utf-8'))
 
 sock.close()
